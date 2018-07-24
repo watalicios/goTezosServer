c071ab41089774760c291537e16b280b46f86b49
DefinitelyNotAGoat <tezosredemption@protonmail.com>
2018-07-24 17:13:08 -0400

    Adding a new backend with mongodb, untested, not compiling

--- tezosMongo.go
+++ tezosMongo.go
@@ -160,7 +160,7 @@ Returns (string): Returns the output of the executed command as a string
 */
 func MongoAddBlock(json string) (error){
   fmt.Println(json)
-  json = "[" + json "]"
+  json = "[" + json + "]"
   out, err := exec.Command("mongoimport", "--db", "TEZOS", "--collection", "blocks", "--type", "json", json).Output()
   if err != nil {
     return  err
