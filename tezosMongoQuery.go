4b441edea0988206021fa4afe60e1d9c05e73ed5
DefinitelyNotAGoat <tezosredemption@protonmail.com>
2018-07-25 18:28:03 -0400

    Adding Get Functions

--- tezosMongoQuery.go
+++ tezosMongoQuery.go
@@ -50,187 +50,343 @@ func GetBlockHead() (Block, error){  //db.blocks.find().skip(db.blocks.count() -
   return result, nil
 }
 
-// func GetBlockProtocol(arg interface{}) (string, error){
+func GetBlockProtocol(arg interface{}) (string, error){
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Protocol, nil
+}
+
+func GetBlockChainId(arg interface{}) (string, error){
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.ChainID, nil
+}
+
+func GetBlockHash(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Hash, nil
+}
+
+// func GetBlockHeader(arg interface{}) (Block.Header, error) {
+//   block, err := GetBlock(arg)
+//   if (err != nil){
+//     return "", err
+//   }
+//   return block.Header, nil
+// }
+
+
+func GetBlockHeaderLevel(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Level, nil
+}
+
+func GetBlockHeaderProto(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Proto, nil
+}
+
+func GetBlockHeaderPredecessor(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Predecessor, nil
+}
+
+func GetBlockHeaderTimeStamp(arg interface{}) (Time.time, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Timestamp, nil
+}
+
+func GetBlockHeaderValidationPass(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.ValidationPass, nil
+}
+
+func GetBlockHeaderOperationsHash(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.OperationsHash, nil
+}
+
+func GetBlockHeaderFitness(arg interface{}) ([]string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Fitness, nil
+}
+
+func GetBlockHeaderContext(arg interface{}) ([]string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Context, nil
+}
+
+func GetBlockHeaderPriority(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.Priority, nil
+}
+
+func GetBlockHeaderProofOfWorkNonce(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.ProofOfWorkNonce, nil
+}
+
+func GetBlockHeaderSignature(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Header.ProofOfWorkNonce, nil
+}
+
+// func GetBlockMetadata(arg interface{}) (Block.Metadata, error) {
 //
 // }
-//
-// func GetBlockChainId(arg interface{}) (string, error){
-//
-// }
-//
-// func GetBlockHash(arg interface{}) (string, error) {
-//
-// }
-//
-// // func GetBlockHeader(arg interface{}) (Block.Header, error) {
-// //
-// // }
-//
-//
-// func GetBlockHeaderLevel(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockHeaderProto(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockHeaderPredecessor(arg interface{}) (string, error) {
+
+func GetBlockMetadataProtocol(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Protocol, nil
+}
+
+func GetBlockMetadataNextProtocol(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.NextProtocol, nil
+}
+
+// func GetBlockMetadataTestChainStatus(arg interface{}) (Block.Metadata.TestChainStatus, error) {
 //
 // }
-//
-// func GetBlockHeaderTimeStamp(arg interface{}) (Time.time, error) {
-//
-// }
-//
-// func GetBlockHeaderValidationPass(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockHeaderOperationsHash(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockHeaderFitness(arg interface{}) ([]string, error) {
-//
-// }
-//
-// func GetBlockHeaderContext(arg interface{}) ([]string, error) {
-//
-// }
-//
-// func GetBlockHeaderPriority(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockHeaderProofOfWorkNonce(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockHeaderSignature(arg interface{}) (string, error) {
-//
-// }
-//
-// // func GetBlockMetadata(arg interface{}) (Block.Metadata, error) {
-// //
-// // }
-//
-// func GetBlockMetadataProtocol(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataNextProtocol(arg interface{}) (string, error) {
-//
-// }
-//
-// // func GetBlockMetadataTestChainStatus(arg interface{}) (Block.Metadata.TestChainStatus, error) {
-// //
-// // }
-//
-// func GetBlockMetadataMaxOperationsTTL(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataMaxOperationDataLength(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataMaxBlockHeaderLength(arg interface{}) (int, error) {
-//
-// }
-//
+
+func GetBlockMetadataMaxOperationsTTL(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.MaxOperationsTTL, nil
+}
+
+func GetBlockMetadataMaxOperationDataLength(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.MaxOperationDataLength, nil
+}
+
+func GetBlockMetadataMaxBlockHeaderLength(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.MaxBlockHeaderLength, nil
+}
+
 // // func GetBlockMetadataMaxOperationListLength(arg interface{}) ([]Block.Metadata.MaxOperationListLength, error) {
 // //
 // // }
 //
-// func GetBlockMetadataMaxOperationDataLengthMaxSize(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataMaxOperationDataLengthMaxOp(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataBaker(arg interface{}) (string, error) {
-//
-// }
-//
+func GetBlockMetadataMaxOperationDataLengthMaxSize(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.MaxOperationListLength.MaxSize, nil
+}
+
+func GetBlockMetadataMaxOperationDataLengthMaxOp(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.MaxOperationListLength.MaxOP, nil
+}
+
+func GetBlockMetadataBaker(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Baker, nil
+}
+
 // // func GetBlockMetadataLevel(arg interface{}) ([]Block.Metadata.Level, error) {
 // //
 // // }
 //
-// func GetBlockMetadataLevelLevel(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataLevelPosition(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataLevelCycle(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataLevelCyclePosition(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataLevelVotingPeriod(arg interface{}) (int, error) {
-//
-// }
-//
-// func GetBlockMetadataLevelExpectedCommitment(arg interface{}) (bool, error) {
-//
-// }
-//
-// func GetBlockMetadataVotingPeriodKind(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataNonceHash(arg interface{}) (interface{}, error) {
-//
-// }
-//
-// func GetBlockMetadataConsumedGas(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataDeactivated(arg interface{}) (interface{}, error) {
-//
-// }
-//
+func GetBlockMetadataLevelLevel(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.Level, nil
+}
+
+func GetBlockMetadataLevelPosition(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.Position, nil
+}
+
+func GetBlockMetadataLevelCycle(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.Position, nil
+}
+
+func GetBlockMetadataLevelCyclePosition(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.CyclePosition, nil
+}
+
+func GetBlockMetadataLevelVotingPeriod(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.VotingPeriod, nil
+}
+
+func GetBlockMetadataLevelExpectedCommitment(arg interface{}) (bool, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Level.ExpectedCommitment, nil
+}
+
+func GetBlockMetadataVotingPeriodKind(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.VotingPeriodKind, nil
+}
+
+func GetBlockMetadataNonceHash(arg interface{}) (interface{}, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.NonceHash, nil
+}
+
+func GetBlockMetadataConsumedGas(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.ConsumedGas, nil
+}
+
+func GetBlockMetadataDeactivated(arg interface{}) (interface{}, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Deactivated, nil
+}
+
 // // func GetBlockMetadataBalanceUpdates(arg interface{}) ([]Block.Metadata.BalanceUpdates, error) {
 // //
 // // }
 //
-// func GetBlockMetadataBalanceKind(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataBalanceUpdatesContract(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataBalanceUpdatesChange(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataBalanceUpdatesCategory(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataBalanceUpdatesDelegate(arg interface{}) (string, error) {
-//
-// }
-//
-// func GetBlockMetadataBalanceUpdatesLevel(arg interface{}) (int, error) {
-//
-// }
-//
+func GetBlockMetadataBalanceKind(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.Balance.Kind, nil
+}
+
+func GetBlockMetadataBalanceUpdatesContract(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.BalanceUpdates.Contract, nil
+}
+
+func GetBlockMetadataBalanceUpdatesChange(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.BalanceUpdates.Change, nil
+}
+
+func GetBlockMetadataBalanceUpdatesCategory(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.BalanceUpdates.Category, nil
+}
+
+func GetBlockMetadataBalanceUpdatesDelegate(arg interface{}) (string, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.BalanceUpdates.Delegate, nil
+}
+
+func GetBlockMetadataBalanceUpdatesLevel(arg interface{}) (int, error) {
+  block, err := GetBlock(arg)
+  if (err != nil){
+    return "", err
+  }
+  return block.Metadata.BalanceUpdates.Level, nil
+}
+
 // // func GetBlockOperations(arg interface{}) (Block.Operations, error){
 // //
 // // }
-//
+
 // func GetBlockOperationsProtocol(arg interface{}) (string, error){
 //
 // }
@@ -242,7 +398,7 @@ func GetBlockHead() (Block, error){  //db.blocks.find().skip(db.blocks.count() -
 // func GetBlockOperationsHash(arg interface{}) (string, error){
 //
 // }
-//
+
 // func GetBlockOperationsBranch(arg interface{}) (string, error){
 //
 // }
