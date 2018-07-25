98ba8e5544d9a0eca5653f380d7d532e1ebbae91
DefinitelyNotAGoat <tezosredemption@protonmail.com>
2018-07-25 12:58:25 -0400

    Defining Get Methods

---
+++ tezosMongoQuery.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+  "time"
+)
+
+
+func GetBlock(arg interface{}) (Block, error){
+
+}
+
+func GetBlockHead() (Block, error){
+
+}
+
+func GetBlockProtocol(arg interface{}) (string, error){
+
+}
+
+func GetBlockChainId(arg interface{}) (string, error){
+
+}
+
+func GetBlockHash(arg interface{}) (string, error) {
+
+}
+
+func GetBlockHeader(arg interface{}) (Block.Header, error) {
+
+}
+
+
+func GetBlockHeaderLevel(arg interface{}) (int, error) {
+
+}
+
+func GetBlockHeaderProto(arg interface{}) (int, error) {
+
+}
+
+func GetBlockHeaderPredecessor(arg interface{}) (string, error) {
+
+}
+
+func GetBlockHeaderTimeStamp(arg interface{}) (Time.time, error) {
+
+}
+
+func GetBlockHeaderValidationPass(arg interface{}) (int, error) {
+
+}
+
+func GetBlockHeaderOperationsHash(arg interface{}) (string, error) {
+
+}
+
+func GetBlockHeaderFitness(arg interface{}) ([]string, error) {
+
+}
+
+func GetBlockHeaderContext(arg interface{}) ([]string, error) {
+
+}
+
+func GetBlockHeaderPriority(arg interface{}) (int, error) {
+
+}
+
+func GetBlockHeaderProofOfWorkNonce(arg interface{}) (int, error) {
+
+}
+
+func GetBlockHeaderSignature(arg interface{}) (string, error) {
+
+}
+
+func GetBlockMetadata(arg interface{}) (Block.Metadata, error) {
+
+}
+
+func GetBlockMetadata(arg interface{}) (Block.Metadata, error) {
+
+}
+
+func GetBlockMetadataProtocol(arg interface{}) (string, error) {
+
+}
+
+func GetBlockMetadataNextProtocol(arg interface{}) (string, error) {
+
+}
+
+func GetBlockMetadataTestChainStatus(arg interface{}) (Block.Metadata.TestChainStatus, error) {
+
+}
+
+func GetBlockMetadataTestChainStatus(arg interface{}) (Block.Metadata.TestChainStatus, error) {
+
+}
+
+func GetBlockMetadataTestChainStatusStatus(arg interface{}) (string, error) {
+
+}
+
+func GetBlockMetadataMaxOperationsTTL(arg interface{}) (int, error) {
+
+}
+
+func GetBlockMetadataMaxOperationDataLength(arg interface{}) (int, error) {
+
+}
+
+func GetBlockMetadataMaxBlockHeaderLength(arg interface{}) (int, error) {
+
+}
+
+func GetBlockMetadataMaxOperationListLength(arg interface{}) ([]Block.Metadata.MaxOperationListLength, error) {
+
+}
+
+func GetBlockMetadataMaxOperationDataLength(arg interface{}) (int, error) {
+
+}
