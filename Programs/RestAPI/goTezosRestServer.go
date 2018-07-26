ea0d8639009949673af3bb2b26e0f7992e52fea6
DefinitelyNotAGoat <tezosredemption@protonmail.com>
2018-07-26 16:45:07 -0400

    rest work

--- Programs/RestAPI/goTezosRestServer.go
+++ Programs/RestAPI/goTezosRestServer.go
@@ -30,6 +30,15 @@ func main(){
   r.HandleFunc("/block/priority/{id}", GetBlockPriority).Methods("GET")
   r.HandleFunc("/block/proofofworknonce/{id}", GetBlockProofOfWorkNonce).Methods("GET")
   r.HandleFunc("/block/signature/{id}", GetBlockSignature).Methods("GET")
+  r.HandleFunc("/block/metadata/{id}", GetBlockMetadata).Methods("GET")
+  r.HandleFunc("/block/metadata/protocol/{id}", GetBlockMetadataProtocol).Methods("GET")
+  r.HandleFunc("/block/metadata/nextprotocol/{id}", GetBlockMetadataNextProtocol).Methods("GET")
+  r.HandleFunc("/block/metadata/testchainstatus/{id}", GetBlockMetadataTestChainStatus).Methods("GET")
+  r.HandleFunc("/block/metadata/maxoperationsttl/{id}", GetBlockMetadataMaxOperationsTTL).Methods("GET")
+  r.HandleFunc("/block/metadata/maxoperationdatalength/{id}", GetBlockMetadataMaxOperationDataLength).Methods("GET")
+  r.HandleFunc("/block/metadata/maxblockheaderlength/{id}", GetBlockMetadataMaxBlockHeaderLength).Methods("GET")
+  r.HandleFunc("/block/metadata/maxoperationlistlength/{id}", GetBlockMetadataMaxOperationListLength).Methods("GET")
+  //GetBlockMetadataMaxOperationListLength
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
@@ -395,6 +404,182 @@ func GetBlockSignature(w http.ResponseWriter, r *http.Request){
     rtnSig = sig
   }
   respondWithJson(w, http.StatusOK, rtnSig)
+}
+
+func GetBlockMetadata(w http.ResponseWriter, r *http.Request){
+  var rtnMeta StructMetadata
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    meta, err := goTezosServer.GetBlockMetadata(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMeta = meta
+  } else {
+    meta, err := goTezosServer.GetBlockMetadata(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMeta = meta
+  }
+  respondWithJson(w, http.StatusOK, rtnSig)
+}
+
+func GetBlockMetadataProtocol(w http.ResponseWriter, r *http.Request){
+  var rtnProtocol string
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    protocol, err := goTezosServer.GetBlockMetadataProtocol(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnProtocol = protocol
+  } else {
+    protocol, err := goTezosServer.GetBlockMetadataProtocol(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnProtocol = protocol
+  }
+  respondWithJson(w, http.StatusOK, rtnProtocol)
+}
+
+func GetBlockMetadataNextProtocol(w http.ResponseWriter, r *http.Request){
+  var rtnProtocol string
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    protocol, err := goTezosServer.GetBlockMetadataNextProtocol(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnProtocol = protocol
+  } else {
+    protocol, err := goTezosServer.GetBlockMetadataNextProtocol(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnProtocol = protocol
+  }
+  respondWithJson(w, http.StatusOK, rtnProtocol)
+}
+
+func GetBlockMetadataTestChainStatus(w http.ResponseWriter, r *http.Request){
+  var rtnTest StructTestChainStatus
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    test, err := goTezosServer.GetBlockMetadataTestChainStatus(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnTest = test
+  } else {
+    test, err := goTezosServer.GetBlockMetadataTestChainStatus(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnTest = test
+  }
+  respondWithJson(w, http.StatusOK, rtnTest)
+}
+
+func GetBlockMetadataMaxOperationsTTL(w http.ResponseWriter, r *http.Request){
+  var rtnMaxOperationsTTL StructTestChainStatus
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    maxOperationsTTL, err := goTezosServer.GetBlockMetadataMaxOperationsTTL(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationsTTL = maxOperationsTTL
+  } else {
+    maxOperationsTTL, err := goTezosServer.GetBlockMetadataMaxOperationsTTL(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationsTTL = maxOperationsTTL
+  }
+  respondWithJson(w, http.StatusOK, rtnMaxOperationsTTL)
+}
+
+func GetBlockMetadataMaxOperationDataLength(w http.ResponseWriter, r *http.Request){
+  var rtnMaxOperationDataLength StructTestChainStatus
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    maxOperationDataLength, err := goTezosServer.GetBlockMetadataMaxOperationDataLength(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationDataLength = maxOperationDataLength
+  } else {
+    maxOperationDataLength, err := goTezosServer.GetBlockMetadataMaxOperationDataLength(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationDataLength = maxOperationDataLength
+  }
+  respondWithJson(w, http.StatusOK, rtnMaxOperationDataLength)
+}
+
+func GetBlockMetadataMaxBlockHeaderLength(w http.ResponseWriter, r *http.Request){
+  var rtnMaxBlockHeaderLength StructTestChainStatus
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    maxBlockHeaderLength, err := goTezosServer.GetBlockMetadataMaxBlockHeaderLength(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxBlockHeaderLength = maxBlockHeaderLength
+  } else {
+    maxBlockHeaderLength, err := goTezosServer.GetBlockMetadataMaxBlockHeaderLength(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxBlockHeaderLength = maxBlockHeaderLength
+  }
+  respondWithJson(w, http.StatusOK, rtnMaxBlockHeaderLength)
+}
+
+func GetBlockMetadataMaxOperationListLength(w http.ResponseWriter, r *http.Request){
+  var rtnMaxOperationListLength StructTestChainStatus
+  params := mux.Vars(r)
+  blockid, isInt := strconv.Atoi(params["id"])
+  if (isInt != nil){
+    maxOperationListLength, err := goTezosServer.GetBlockMetadataMaxOperationListLength(params["id"])
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationListLength = maxOperationListLength
+  } else {
+    maxBlockHeaderLength, err := goTezosServer.GetBlockMetadataMaxOperationListLength(blockid)
+    if err != nil {
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+      return
+    }
+    rtnMaxOperationListLength = maxOperationListLength
+  }
+  respondWithJson(w, http.StatusOK, rtnMaxOperationListLength)
 }
 // func CheckType(v interface{}) (int, error) {
 //   switch v.(type){
