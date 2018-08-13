3946ad3755cf0d6793c6c4a3da69f8ecbf616564
DefinitelyNotAGoat <definitelynotagoat@protonmail.com>
2018-08-13 11:04:05 -0400

    Adding snapshot calc

--- goTezosServerStructs.go
+++ goTezosServerStructs.go
@@ -1,130 +1,143 @@
 package goTezosServer
 
 import (
-  "time"
+	"time"
 )
 
-type Block struct{
-  Protocol    string              `json:"protocol"`
-	ChainID     string              `json:"chain_id"`
-	Hash        string              `json:"hash"`
-  Header      StructHeader        `json:"header"`
-  Metadata    StructMetadata      `json:"metadata"`
-  Operations  [][]StructOperations  `json:"operations"`
+type Block struct {
+	Protocol   string               `json:"protocol"`
+	ChainID    string               `json:"chain_id"`
+	Hash       string               `json:"hash"`
+	Header     StructHeader         `json:"header"`
+	Metadata   StructMetadata       `json:"metadata"`
+	Operations [][]StructOperations `json:"operations"`
 }
 
 type StructHeader struct {
-  Level            int           `json:"level"`
-  Proto            int           `json:"proto"`
-  Predecessor      string        `json:"Predecessor"`
-  Timestamp        time.Time     `json:"timestamp"`
-  ValidationPass   int           `json:"validation_pass"`
-  OperationsHash   string        `json:"operations_hash"`
-  Fitness          []string      `json:"fitness"`
-  Context          string        `json:"context"`
-  Priority         int           `json:"priority"`
-  ProofOfWorkNonce string        `json:"proof_of_work_nonce"`
-  Signature        string        `json:"signature"`
+	Level            int       `json:"level"`
+	Proto            int       `json:"proto"`
+	Predecessor      string    `json:"Predecessor"`
+	Timestamp        time.Time `json:"timestamp"`
+	ValidationPass   int       `json:"validation_pass"`
+	OperationsHash   string    `json:"operations_hash"`
+	Fitness          []string  `json:"fitness"`
+	Context          string    `json:"context"`
+	Priority         int       `json:"priority"`
+	ProofOfWorkNonce string    `json:"proof_of_work_nonce"`
+	Signature        string    `json:"signature"`
 }
 
 type StructMetadata struct {
-  Protocol                 string                         `json:"protocol"`
-  NextProtocol             string                         `json:"next_protocol"`
-  TestChainStatus          StructTestChainStatus          `json:"test_chain_status"`
-  MaxOperationsTTL         int                            `json:"max_operations_ttl"`
-  MaxOperationDataLength   int                            `json:"max_operation_data_length"`
-  MaxBlockHeaderLength     int                            `json:"max_block_header_length"`
-  MaxOperationListLength   []StructMaxOperationListLength `json:"max_operation_list_length"`
-  Baker                    string                         `json:"baker"`
-  Level                    StructLevel                    `json:"level"`
-  VotingPeriodKind         string                         `json:"voting_period_kind"`
-  NonceHash                interface{}                    `json:"nonce_hash"`
-  ConsumedGas              string                         `json:"consumed_gas"`
-  Deactivated              []string                       `json:"deactivated"`
-  BalanceUpdates           []StructBalanceUpdates         `json:"balance_updates"`
+	Protocol               string                         `json:"protocol"`
+	NextProtocol           string                         `json:"next_protocol"`
+	TestChainStatus        StructTestChainStatus          `json:"test_chain_status"`
+	MaxOperationsTTL       int                            `json:"max_operations_ttl"`
+	MaxOperationDataLength int                            `json:"max_operation_data_length"`
+	MaxBlockHeaderLength   int                            `json:"max_block_header_length"`
+	MaxOperationListLength []StructMaxOperationListLength `json:"max_operation_list_length"`
+	Baker                  string                         `json:"baker"`
+	Level                  StructLevel                    `json:"level"`
+	VotingPeriodKind       string                         `json:"voting_period_kind"`
+	NonceHash              interface{}                    `json:"nonce_hash"`
+	ConsumedGas            string                         `json:"consumed_gas"`
+	Deactivated            []string                       `json:"deactivated"`
+	BalanceUpdates         []StructBalanceUpdates         `json:"balance_updates"`
 }
 
 type StructTestChainStatus struct {
-  Status string            `json:"status"`
+	Status string `json:"status"`
 }
 
 type StructMaxOperationListLength struct {
-  MaxSize int              `json:"max_size"`
-  MaxOp   int              `json:"max_op,omitempty"`
+	MaxSize int `json:"max_size"`
+	MaxOp   int `json:"max_op,omitempty"`
 }
 
 type StructLevel struct {
-  Level                int              `json:"level"`
-  LevelPosition        int              `json:"level_position"`
-  Cycle                int              `json:"cycle"`
-  CyclePosition        int              `json:"cycle_position"`
-  VotingPeriod         int              `json:"voting_period"`
-  VotingPeriodPosition int              `json:"voting_period_position"`
-  ExpectedCommitment   bool             `json:"expected_commitment"`
+	Level                int  `json:"level"`
+	LevelPosition        int  `json:"level_position"`
+	Cycle                int  `json:"cycle"`
+	CyclePosition        int  `json:"cycle_position"`
+	VotingPeriod         int  `json:"voting_period"`
+	VotingPeriodPosition int  `json:"voting_period_position"`
+	ExpectedCommitment   bool `json:"expected_commitment"`
 }
 
 type StructBalanceUpdates struct {
-  Kind     string           `json:"kind"`
-  Contract string           `json:"contract,omitempty"`
-  Change   string           `json:"change"`
-  Category string           `json:"category,omitempty"`
-  Delegate string           `json:"delegate,omitempty"`
-  Level    int              `json:"level,omitempty"`
+	Kind     string `json:"kind"`
+	Contract string `json:"contract,omitempty"`
+	Change   string `json:"change"`
+	Category string `json:"category,omitempty"`
+	Delegate string `json:"delegate,omitempty"`
+	Level    int    `json:"level,omitempty"`
 }
 
 type StructOperations struct {
-  Protocol  string           `json:"protocol"`
-  ChainID   string           `json:"chain_id"`
-  Hash      string           `json:"hash"`
-  Branch    string           `json:"branch"`
-  Contents  []StructContents `json:"contents"`
-  Signature string           `json:"signature"`
+	Protocol  string           `json:"protocol"`
+	ChainID   string           `json:"chain_id"`
+	Hash      string           `json:"hash"`
+	Branch    string           `json:"branch"`
+	Contents  []StructContents `json:"contents"`
+	Signature string           `json:"signature"`
 }
 
 type StructContents struct {
-  Kind     string             `json:"kind"`
-  Source   string             `json:"source"`
-  Fee      string             `json:"fee"`
-  Counter  string             `json:"counter"`
-  GasLimit  string            `json:"gas_limit"`
-  StorageLimit string         `json:"storage_limit"`
-  Amount string               `json:"amount"`
-  Destination string          `json:"destination"`
-  Delegate string             `json:"delegate"`
-  Phk      string             `json:"phk"`
-  Secret   string             `json:"secret"`
-  Level    int                `json:"level"`
-  ManagerPublicKey string     `json:"managerPubkey"`
-  Balance string              `json:"balance"`
-  Metadata ContentsMetadata   `json:"metadata"`
+	Kind             string           `json:"kind"`
+	Source           string           `json:"source"`
+	Fee              string           `json:"fee"`
+	Counter          string           `json:"counter"`
+	GasLimit         string           `json:"gas_limit"`
+	StorageLimit     string           `json:"storage_limit"`
+	Amount           string           `json:"amount"`
+	Destination      string           `json:"destination"`
+	Delegate         string           `json:"delegate"`
+	Phk              string           `json:"phk"`
+	Secret           string           `json:"secret"`
+	Level            int              `json:"level"`
+	ManagerPublicKey string           `json:"managerPubkey"`
+	Balance          string           `json:"balance"`
+	Metadata         ContentsMetadata `json:"metadata"`
 }
 
 type ContentsMetadata struct {
-  BalanceUpdates []StructBalanceUpdates `json:"balance_updates"`
-  Slots    []int                        `json:"slots"`
+	BalanceUpdates []StructBalanceUpdates `json:"balance_updates"`
+	Slots          []int                  `json:"slots"`
 }
 
-type StructDelegate struct{
-  Balance string  `json:"balance"`
-  FrozenBalance string `json:"frozen_balance"`
-  FrozenBalanceByCycle []StructFrozenBalanceByCycle `json:"frozen_balance_by_cycle"`
-  StakingBalance string `json:"staking_balance"`
-  DelegateContracts []string  `json:"delegated_contracts"`
-  DelegatedBalance string `json:"delegated_balance"`
-  Deactivated bool  `json:"deactivated"`
-  GracePeriod int `json:"grace_period"`
+type StructDelegate struct {
+	Balance              string                       `json:"balance"`
+	FrozenBalance        string                       `json:"frozen_balance"`
+	FrozenBalanceByCycle []StructFrozenBalanceByCycle `json:"frozen_balance_by_cycle"`
+	StakingBalance       string                       `json:"staking_balance"`
+	DelegateContracts    []string                     `json:"delegated_contracts"`
+	DelegatedBalance     string                       `json:"delegated_balance"`
+	Deactivated          bool                         `json:"deactivated"`
+	GracePeriod          int                          `json:"grace_period"`
 }
 
-type StructFrozenBalanceByCycle struct{
-  Cycle int `json:"cycle"`
-  Deposit string `json:"deposit"`
-  Fees string `json:"fees"`
-  Rewards string `json:"rewards"`
+type StructFrozenBalanceByCycle struct {
+	Cycle   int    `json:"cycle"`
+	Deposit string `json:"deposit"`
+	Fees    string `json:"fees"`
+	Rewards string `json:"rewards"`
 }
 
+// type SnapShot struct {
+//   Cycle int
+//   RandomSeed string
+//   Number string
+//   Level int
+// }
+
+/*
+Description: A structure to hold a snapshot query in.
+Cycle (int): The cycle the snapshot was taken in
+Number (int): Snap shot number decided. Empty if decided == false
+Decided (bool): true or false if snap shot is decided
+AssociatedBlock: The block number the snapshot reference, only available if snapshot is decided.
+*/
 type SnapShot struct {
-  Cycle int
-  RandomSeed string
-  Number string
-  Level int
+	Cycle           int
+	Number          int
+	AssociatedBlock int
 }
--- tezosMongo.go
+++ tezosMongo.go
@@ -8,20 +8,23 @@ License: MIT
 */
 
 import (
-  "fmt"
-  "os"
-  "os/exec"
-  "errors"
-  "regexp"
-  "strconv"
-  "time"
-  "encoding/json"
-  "gopkg.in/mgo.v2"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"time"
+
+	"gopkg.in/mgo.v2"
 )
 
 var (
-  reGetBlockLevelHead = regexp.MustCompile(`"level": ([0-9]+), "proto"`)
-  reGetHash = regexp.MustCompile(`"hash": "([0-9a-zA-Z]+)",`)
+	reGetBlockLevelHead = regexp.MustCompile(`"level": ([0-9]+), "proto"`)
+	reGetHash           = regexp.MustCompile(`"hash": "([0-9a-zA-Z]+)",`)
+	reGetRandomSeed     = regexp.MustCompile(`"random_seed":\n[ ]+ "([0-9a-zA-Z]+)"`)
+	reGetRollSnapShot   = regexp.MustCompile(`"roll_snapshot": ([0-9]+)`)
 )
 
 var TezosPath string
@@ -32,120 +35,120 @@ var Collection *mgo.Collection
 Description: This library needs the TEZOSPATH enviroment variable to function
 */
 func init() {
-  var ok bool
-  TezosPath, ok = os.LookupEnv("TEZOSPATH")
-  if !ok {
-	   fmt.Println("TEZOSPATH not set. Please 'export TEZOSPATH=<path_to_tezos>'.")
-	   os.Exit(1)
-  }
-  TezosPath = TezosPath + "tezos-client"
-  if _, err := os.Stat(TezosPath); os.IsNotExist(err) {
-    fmt.Println("Could not find tezos-client in TEZOSPATH: " + err.Error())
-    os.Exit(1)
-  }
+	var ok bool
+	TezosPath, ok = os.LookupEnv("TEZOSPATH")
+	if !ok {
+		fmt.Println("TEZOSPATH not set. Please 'export TEZOSPATH=<path_to_tezos>'.")
+		os.Exit(1)
+	}
+	TezosPath = TezosPath + "tezos-client"
+	if _, err := os.Stat(TezosPath); os.IsNotExist(err) {
+		fmt.Println("Could not find tezos-client in TEZOSPATH: " + err.Error())
+		os.Exit(1)
+	}
 }
 
-func SetDatabaseConnection(connection ,database, collection string){
-  Session, err := mgo.Dial(connection)
-  if (err != nil){
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  Collection = Session.DB(database).C(collection)
+func SetDatabaseConnection(connection, database, collection string) {
+	Session, err := mgo.Dial(connection)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	Collection = Session.DB(database).C(collection)
 }
 
-func InitSynchronizeTezosMongo(){
-  err := MongoGetAllBlocks()
-  if (err != nil){
-    fmt.Println(err)
-  }
+func InitSynchronizeTezosMongo() {
+	err := MongoGetAllBlocks()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func SynchronizeTezosMongo() {
-  blockDb, _ := GetBlockHead() //Get last block in db
-  level :=  blockDb.Header.Level
-  nextLevel := level +1
-  run := true
+	blockDb, _ := GetBlockHead() //Get last block in db
+	level := blockDb.Header.Level
+	nextLevel := level + 1
+	run := true
 
-  for run == true{
-    tmpBlock, _ := GetBlockRPCHead() //Get current head in rpc
-    blockHead := ConvertBlockToJson(tmpBlock)
+	for run == true {
+		tmpBlock, _ := GetBlockRPCHead() //Get current head in rpc
+		blockHead := ConvertBlockToJson(tmpBlock)
 
-    if (nextLevel <= blockHead.Header.Level){
-      block, err := GetBlockRPC(nextLevel, blockHead.Hash, blockHead.Header.Level)
-      if (err != nil){
-        fmt.Println(err)
-      }
-      err = Collection.Insert(block)
-      if (err != nil){
-        fmt.Println(err)
-      }
-      nextLevel = nextLevel + 1
-    }
-    time.Sleep(1 * time.Second)
-  }
+		if nextLevel <= blockHead.Header.Level {
+			block, err := GetBlockRPC(nextLevel, blockHead.Hash, blockHead.Header.Level)
+			if err != nil {
+				fmt.Println(err)
+			}
+			err = Collection.Insert(block)
+			if err != nil {
+				fmt.Println(err)
+			}
+			nextLevel = nextLevel + 1
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
 
-func MongoGetAllBlocks() error{
-  head, err := GetBlockRPCHead()
-  if (err != nil){
-    return err
-  }
+func MongoGetAllBlocks() error {
+	head, err := GetBlockRPCHead()
+	if err != nil {
+		return err
+	}
 
-  regHeadLevelResult := reGetBlockLevelHead.FindStringSubmatch(string(head[:]))
-  if (regHeadLevelResult == nil){
-    return errors.New("Could not get block level for head")
-  }
-  headLevel, _ := strconv.Atoi(regHeadLevelResult[1]) //TODO Error Checking
-  headHash := reGetHash.FindStringSubmatch(string(head[:])) //TODO Error check the regex
-  if (headHash == nil){
-    return errors.New("Could not get hash for block head")
-  }
+	regHeadLevelResult := reGetBlockLevelHead.FindStringSubmatch(string(head[:]))
+	if regHeadLevelResult == nil {
+		return errors.New("Could not get block level for head")
+	}
+	headLevel, _ := strconv.Atoi(regHeadLevelResult[1])       //TODO Error Checking
+	headHash := reGetHash.FindStringSubmatch(string(head[:])) //TODO Error check the regex
+	if headHash == nil {
+		return errors.New("Could not get hash for block head")
+	}
 
-  for i := headLevel-headLevel; i < headLevel; i ++{
-    block, err := GetBlockRPC(i, headHash[1], headLevel)
-    if (err != nil){
-      return err
-    }
+	for i := headLevel - headLevel; i < headLevel; i++ {
+		block, err := GetBlockRPC(i, headHash[1], headLevel)
+		if err != nil {
+			return err
+		}
 
-    // for _, op := range block.Operations{
-    //   for _, operation := range op{
-    //     for _, content := range op.Contents{
-    //       if (content.Kind == "delegation"){
-    //         content.Delegate
-    //       }
-    //     }
-    //   }
-    // }
-    err = Collection.Insert(block)
-    if (err != nil){
-      fmt.Println(err)
-    }
-  }
-  return nil
+		// for _, op := range block.Operations{
+		//   for _, operation := range op{
+		//     for _, content := range op.Contents{
+		//       if (content.Kind == "delegation"){
+		//         content.Delegate
+		//       }
+		//     }
+		//   }
+		// }
+		err = Collection.Insert(block)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
 }
 
-func GetBlockRPC(level int, headHash string, headLevel int) (Block, error){
-  diff := headLevel - level
-  diffStr := strconv.Itoa(diff)
-  getBlockByLevel := "chains/main/blocks/" + headHash + "~" + diffStr
-  var block Block
+func GetBlockRPC(level int, headHash string, headLevel int) (Block, error) {
+	diff := headLevel - level
+	diffStr := strconv.Itoa(diff)
+	getBlockByLevel := "chains/main/blocks/" + headHash + "~" + diffStr
+	var block Block
 
-  s, err := TezosRPCGet(getBlockByLevel)
-  if (err != nil){
-    return block, err
-  }
+	s, err := TezosRPCGet(getBlockByLevel)
+	if err != nil {
+		return block, err
+	}
 
-  block = ConvertBlockToJson(s)
-  return block, nil
+	block = ConvertBlockToJson(s)
+	return block, nil
 }
 
-func GetBlockRPCHead() ([]byte, error){
-  s, err := TezosRPCGet("chains/main/blocks/head")
-  if (err != nil){
-    return s, errors.New("Could not get block level for head: TezosRPCGet(arg string) failed: " + err.Error())
-  }
-  return s, nil
+func GetBlockRPCHead() ([]byte, error) {
+	s, err := TezosRPCGet("chains/main/blocks/head")
+	if err != nil {
+		return s, errors.New("Could not get block level for head: TezosRPCGet(arg string) failed: " + err.Error())
+	}
+	return s, nil
 }
 
 /*
@@ -153,13 +156,41 @@ Description: Takes an  array of interface (struct in our case), jsonifies it, an
 Param v (interface{}): Array of an interface
 */
 func ConvertBlockToJson(v []byte) Block {
-  var block Block
-  err := json.Unmarshal(v, &block)
-  if err != nil {
-        panic(err)
-  }
+	var block Block
+	err := json.Unmarshal(v, &block)
+	if err != nil {
+		panic(err)
+	}
 
-  return block
+	return block
+}
+
+func GetSnapShot(cycle int) (SnapShot, error) {
+	var snapShot SnapShot
+	snapShot.Cycle = cycle
+	strCycle := strconv.Itoa(cycle)
+
+	snapshotStr := "/chains/main/blocks/head/context/raw/json/cycle/" + strCycle
+
+	s, err := TezosRPCGet(snapshotStr)
+	if err != nil {
+		return snapShot, errors.New("func GetSnapShot(cycle int) (SnapShot, error) failed: " + err.Error())
+	}
+
+	regRandomSeed := reGetRandomSeed.FindStringSubmatch(string(s[:]))
+	if regRandomSeed == nil {
+		return snapShot, errors.New("No random seed: func GetSnapShot(cycle int) (SnapShot, error) failed.")
+	}
+
+	regRollSnapShot := reGetRollSnapShot.FindStringSubmatch(string(s[:]))
+	if regRollSnapShot == nil {
+		return snapShot, errors.New("Could not parse snapshot: func GetSnapShot(cycle int) (SnapShot, error) failed.")
+	}
+	number, _ := strconv.Atoi(regRollSnapShot[1])
+	snapShot.Number = number
+	snapShot.AssociatedBlock = ((cycle - 7) * 4096) + (number+1)*256
+
+	return snapShot, nil
 }
 
 /*
@@ -167,13 +198,13 @@ Description: A function that executes a command to the tezos-client
 Param args ([]string): Arguments to be executed
 Returns (string): Returns the output of the executed command as a string
 */
-func TezosDo(args ...string) ([]byte, error){
-  out, err := exec.Command(TezosPath, args...).Output()
-  if err != nil {
-    return out, err
-  }
+func TezosDo(args ...string) ([]byte, error) {
+	out, err := exec.Command(TezosPath, args...).Output()
+	if err != nil {
+		return out, err
+	}
 
-  return out, nil
+	return out, nil
 }
 
 /*
@@ -181,18 +212,18 @@ Description: A function that executes an rpc get arg
 Param args ([]string): Arguments to be executed
 Returns (string): Returns the output of the executed command as a string
 */
-func TezosRPCGet(arg string) ([]byte, error){
-  output, err := TezosDo("rpc", "get", arg)
-  if (err != nil){
-    return output, errors.New("Could not rpc get " + arg + " : tezosDo(args ...string) failed: " + err.Error())
-  }
-  return output, nil
+func TezosRPCGet(arg string) ([]byte, error) {
+	output, err := TezosDo("rpc", "get", arg)
+	if err != nil {
+		return output, errors.New("Could not rpc get " + arg + " : tezosDo(args ...string) failed: " + err.Error())
+	}
+	return output, nil
 }
 
 func PrettyReport(v interface{}) string {
-  b, err := json.MarshalIndent(v, "", "  ")
-  if err == nil {
-    return string(b)
-  }
-  return ""
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		return string(b)
+	}
+	return ""
 }
